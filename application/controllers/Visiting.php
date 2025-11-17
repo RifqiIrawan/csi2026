536ec3c7091bcf46682eeaa57852c4ace20bc14e
rifqi <rifqiirawan@gmail.com>
2025-11-18 01:20:41 +0700

    now push revisi

--- application/controllers/Visiting.php
+++ application/controllers/Visiting.php
@@ -31,8 +31,9 @@ class Visiting extends CI_Controller {
         $data["data_carousel"] = $this->M_Login->get_carousel();
         $data["data_video"] = $this->M_Login->get_highlights();
         $data["data_organizer"] = $this->M_Login->get_organizer();
+        $data["data_member"] = $this->M_Login->get_member();
         $data["data_sponsors"] = $this->M_Login->get_sponsors();
-        $data["data_coperation"] = $this->M_Login->get_coperation();
+        $data["data_coperation"] = $this->M_Login->get_coperation();        
 
         $data["data_footer"] = "../Website/assets/img/ftr.jpg";
         $this->load->view('dashboard',$data);
@@ -40,16 +41,22 @@ class Visiting extends CI_Controller {
 
     public function News_Update(){  
         error_reporting(0);
-        $data["data_carousel"] = $this->M_Home->get_carousel();
-        $data["data_menu"] = $this->M_Home->get_menu();
+        $data["data_menu"] = $this->M_Login->get_menu();
+        $data["data_event"] = $this->M_Login->get_event()->row();
+        $data["data_product"] = $this->M_Login->get_product();
+        $data["data_event_value"] = $this->M_Login->get_event_value();
+        $data["data_support"] = $this->M_Login->get_support();
+        $data["data_content1"] = $this->M_Login->get_content1()->row();
         $data["data_profile"] = $this->M_Login->get_profile()->row();
         $data["data_sosmed"] = $this->M_Login->get_sosmed();
-        $data["data_contact1"] = $this->M_Info->get_contact1();
-        $data["data_contact2"] = $this->M_Info->get_contact2();
-        $data["data_contact3"] = $this->M_Info->get_contact3();
-        $data["data_contact_us"] = $this->M_Info->get_contact_us();    
         $data["data_qlink"] = $this->M_Login->get_qlink();
         $data["data_contact"] = $this->M_Login->get_contact();    
+        $data["data_carousel"] = $this->M_Login->get_carousel();
+        $data["data_video"] = $this->M_Login->get_highlights();
+        $data["data_organizer"] = $this->M_Login->get_organizer();
+        $data["data_member"] = $this->M_Login->get_member();
+        $data["data_sponsors"] = $this->M_Login->get_sponsors();
+        $data["data_coperation"] = $this->M_Login->get_coperation();   
 
         $data["data_header_news"] = $this->M_Info->get_header_news();
         $data["data_news_update"] = $this->M_Info->get_news_update();
@@ -60,16 +67,22 @@ class Visiting extends CI_Controller {
 
     public function Hotel_Booking(){
         error_reporting(0);
-        $data["data_carousel"] = $this->M_Home->get_carousel();
-        $data["data_menu"] = $this->M_Home->get_menu();
+        $data["data_menu"] = $this->M_Login->get_menu();
+        $data["data_event"] = $this->M_Login->get_event()->row();
+        $data["data_product"] = $this->M_Login->get_product();
+        $data["data_event_value"] = $this->M_Login->get_event_value();
+        $data["data_support"] = $this->M_Login->get_support();
+        $data["data_content1"] = $this->M_Login->get_content1()->row();
         $data["data_profile"] = $this->M_Login->get_profile()->row();
         $data["data_sosmed"] = $this->M_Login->get_sosmed();
-        $data["data_contact1"] = $this->M_Info->get_contact1();
-        $data["data_contact2"] = $this->M_Info->get_contact2();
-        $data["data_contact3"] = $this->M_Info->get_contact3();
-        $data["data_contact_us"] = $this->M_Info->get_contact_us();    
         $data["data_qlink"] = $this->M_Login->get_qlink();
         $data["data_contact"] = $this->M_Login->get_contact();    
+        $data["data_carousel"] = $this->M_Login->get_carousel();
+        $data["data_video"] = $this->M_Login->get_highlights();
+        $data["data_organizer"] = $this->M_Login->get_organizer();
+        $data["data_member"] = $this->M_Login->get_member();
+        $data["data_sponsors"] = $this->M_Login->get_sponsors();
+        $data["data_coperation"] = $this->M_Login->get_coperation();   
     
         $data["data_hotel"] = $this->M_Info->get_hotel();
         $data["data_footer"] = "../Website/assets/img/ftr.jpg";
@@ -244,7 +257,7 @@ class Visiting extends CI_Controller {
         $data['show_features'] = $show_features;
 
         $data["data_menu"] = $this->M_Login->get_menu();
-
+        $data["data_event"] = $this->M_Login->get_event()->row();
         $data["data_product"] = $this->M_Login->get_product();
         $data["data_event_value"] = $this->M_Login->get_event_value();
         $data["data_support"] = $this->M_Login->get_support();
@@ -256,8 +269,10 @@ class Visiting extends CI_Controller {
         $data["data_carousel"] = $this->M_Login->get_carousel();
         $data["data_video"] = $this->M_Login->get_highlights();
         $data["data_organizer"] = $this->M_Login->get_organizer();
+        $data["data_member"] = $this->M_Login->get_member();
         $data["data_sponsors"] = $this->M_Login->get_sponsors();
-        $data["data_coperation"] = $this->M_Login->get_coperation();
+        $data["data_coperation"] = $this->M_Login->get_coperation();   
+        
         $this->load->view('layouts/header', $data);
         $this->load->view('visiting',$data);
         $this->load->view('layouts/footer', $data);
@@ -271,6 +286,7 @@ class Visiting extends CI_Controller {
     // echo "</pre>";
     // die();
     $data["data_menu"] = $this->M_Login->get_menu();
+    $data["data_event"] = $this->M_Login->get_event()->row();
     $data["data_product"] = $this->M_Login->get_product();
     $data["data_event_value"] = $this->M_Login->get_event_value();
     $data["data_support"] = $this->M_Login->get_support();
@@ -282,8 +298,10 @@ class Visiting extends CI_Controller {
     $data["data_carousel"] = $this->M_Login->get_carousel();
     $data["data_video"] = $this->M_Login->get_highlights();
     $data["data_organizer"] = $this->M_Login->get_organizer();
+    $data["data_member"] = $this->M_Login->get_member();
     $data["data_sponsors"] = $this->M_Login->get_sponsors();
-    $data["data_coperation"] = $this->M_Login->get_coperation();
+    $data["data_coperation"] = $this->M_Login->get_coperation();   
+
     $this->load->view('layouts/header', $data);
     $this->load->view('module/visiting/conferenceschedule',$data);
     $this->load->view('layouts/footer', $data);
